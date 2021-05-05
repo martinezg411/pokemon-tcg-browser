9af8d56e19327430b560ffebc1c56ab7d3a2b19d
Gerardo Martinez <gerardomartinez@Gerardos-MacBook-Air.local>
2021-05-04 19:59:17 -0700

    Added loading screen while collection is being processed

--- src/pages/MyCollection.js
+++ src/pages/MyCollection.js
@@ -2,11 +2,13 @@ import { useContext, useState, useEffect } from 'react';
 import UserContext from '../context/user/UserContext';
 import data from '../constants/data';
 import SetProgressItem from '../components/SetProgressItem';
+import Spinner from '../components/Spinner';
 
 const MyCollection = () => {
   const userContext = useContext(UserContext);
   const { collection } = userContext;
   const [collectionSets, setCollectionSets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // Check user collection to find all sets that have at least one card.
   useEffect(() => {
@@ -37,24 +39,29 @@ const MyCollection = () => {
     });
 
     setCollectionSets(items);
+    setIsLoading(false);
   }, [collection]);
 
-  return (
-    <div className='container mx-auto mt-8 flex flex-col'>
-      {collectionSets.length === 0 ? (
-        <h1 className='text-white text-center font-bold'>
-          Your collection is empty. Add cards and your progress for the
-          appropriate set will be displayed here.
-        </h1>
-      ) : (
-        <div>
-          {collectionSets.map((set) => (
-            <SetProgressItem key={set.id} information={set} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  if (isLoading) {
+    return <Spinner message='Retrieving your collection.' />;
+  } else {
+    return (
+      <div className='container mx-auto mt-8 flex flex-col'>
+        {collectionSets.length === 0 ? (
+          <h1 className='text-white text-center font-bold'>
+            Your collection is empty. Add cards and your progress for the
+            appropriate set will be displayed here.
+          </h1>
+        ) : (
+          <div>
+            {collectionSets.map((set) => (
+              <SetProgressItem key={set.id} information={set} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
 };
 
 export default MyCollection;
