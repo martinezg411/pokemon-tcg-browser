316b9ca3e81069aa7c8b9050c9379cae25908fcc
Gerardo Martinez <gerardomartinez@Gerardos-MacBook-Air.local>
2021-05-18 12:21:56 -0700

    Add loading icon while database is updating

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,9 +1,10 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import UserContext from '../context/user/UserContext';
 
 const CardItem = ({ card, alert, expand }) => {
   const userContext = useContext(UserContext);
   const { isUserAuthenticated, isCardInCollection, add, remove } = userContext;
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = () => {
     expand(card);
@@ -18,6 +19,8 @@ const CardItem = ({ card, alert, expand }) => {
       return;
     }
 
+    setIsLoading(true);
+
     if (isCardInCollection(card)) {
       remove(card).then((result) => {
         alert(result);
@@ -27,32 +30,44 @@ const CardItem = ({ card, alert, expand }) => {
         alert(result);
       });
     }
+    setIsLoading(false);
   };
 
-  const btn = (
-    <div className={'absolute -top-2 -right-2 rounded-md bg-black p-2'}>
-      {isCardInCollection(card) ? (
-        <button
-          className='text-blue-500 hover:text-blue-600 focus:outline-none'
-          onClick={toggleCheck}
-        >
-          <i className='fas fa-check-square fa-lg'></i>
-        </button>
-      ) : (
-        <button
-          className='text-gray-500 hover:text-gray-600 text-xl focus:outline-none'
-          onClick={toggleCheck}
-        >
-          <i className='fas fa-expand fa-lg'></i>
-        </button>
-      )}
-    </div>
-  );
+  let btn = null;
+
+  if (isLoading) {
+    btn = (
+      <div className='absolute -top-2 -right-2 rounded-md bg-black p-2'>
+        <i className='animate-spin fas fa-spinner fa-lg text-white'></i>
+      </div>
+    );
+  } else {
+    btn = (
+      <div className={'absolute -top-2 -right-2 rounded-md bg-black p-2'}>
+        {isCardInCollection(card) ? (
+          <button
+            className='text-blue-500 hover:text-blue-600 focus:outline-none'
+            onClick={toggleCheck}
+          >
+            <i className='fas fa-check-square fa-lg'></i>
+          </button>
+        ) : (
+          <button
+            className='text-gray-500 hover:text-gray-600 text-xl focus:outline-none'
+            onClick={toggleCheck}
+          >
+            <i className='fas fa-expand fa-lg'></i>
+          </button>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div>
       <div className='relative'>
         {isUserAuthenticated() && btn}
+
         <img
           src={card.images.small}
           className={`mx-auto ${
